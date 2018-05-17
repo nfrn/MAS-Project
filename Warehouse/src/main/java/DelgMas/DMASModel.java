2ba9d8ebcb7dbb17ffa9fe67cf5ed05b14e185a9
Nelson Nunes <nfrn@hotmail.com>
2018-05-17 10:26:56 +0200

    dmas 10%

--- Warehouse/src/main/java/DelgMas/DMASModel.java
+++ Warehouse/src/main/java/DelgMas/DMASModel.java
@@ -1,40 +1,57 @@
 package DelgMas;
 
-import com.github.rinde.rinsim.core.SimulatorAPI;
-import com.github.rinde.rinsim.core.SimulatorUser;
 import com.github.rinde.rinsim.core.model.DependencyProvider;
 import com.github.rinde.rinsim.core.model.Model;
 import com.github.rinde.rinsim.core.model.ModelBuilder;
-import com.github.rinde.rinsim.core.model.pdp.PDPObject;
 import com.github.rinde.rinsim.core.model.road.GraphRoadModel;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
-import com.github.rinde.rinsim.core.model.road.RoadModels;
 import com.github.rinde.rinsim.core.model.time.TickListener;
 import com.github.rinde.rinsim.core.model.time.TimeLapse;
 import com.github.rinde.rinsim.geom.Point;
 import com.google.auto.value.AutoValue;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Map;
+import java.util.*;
 
 public class DMASModel implements TickListener, Model<Point> {
 
     private RoadModel rm;
     private AgvModel am;
     private GraphRoadModel grm;
-    private Map<Point, PheromoneStorage> nodes;
+    private HashMap<Point, PheromoneStorage> nodes;
 
     public DMASModel(RoadModel roadModel, AgvModel agvModel, GraphRoadModel grm) {
         this.rm = roadModel;
         this.am = agvModel;
         this.grm = grm;
+        this.nodes = new HashMap<Point, PheromoneStorage>();
 
+        for(Point p : grm.getGraph().getNodes()){
+            this.nodes.put(p,new PheromoneStorage(100));
+        }
 
     }
 
-
+    static DMASModel.Builder builder() {
+        return new AutoValue_DMASModel_Builder();
+    }
 
     @Override
     public void tick(TimeLapse timeLapse) {
+
+        Iterator<HashMap.Entry<Point, PheromoneStorage>> itr = this.nodes.entrySet().iterator();
+        while(itr.hasNext())
+        {
+            Map.Entry<Point, PheromoneStorage> entry = itr.next();
+            PheromoneStorage p = entry.getValue();
+            int result = p.timepassing(1);
+            if( result == -1){
+                entry.setValue(new PheromoneStorage(100));
+            }
+            else{
+                entry.setValue(p);
+            }
+        }
 
     }
 
@@ -63,9 +80,6 @@ public class DMASModel implements TickListener, Model<Point> {
         return null;
     }
 
-    static DMASModel.Builder builder() {
-        return new AutoValue_DMASModel_Builder();
-    }
     @AutoValue
     abstract static class Builder
             extends ModelBuilder.AbstractModelBuilder<DMASModel, Point> {
--- Warehouse/src/main/java/DelgMas/PheromoneStorage.java
+++ Warehouse/src/main/java/DelgMas/PheromoneStorage.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 
 public class PheromoneStorage {
     public ArrayList<TimeWindow> booked;
+    public int lifetime_booking;
     public ArrayList<TimeWindow> chargerSpots;
-    public int lifetime;
+    public int lifetime_charger;
 
     public PheromoneStorage(int lifetime){
-        this.lifetime=lifetime;
+        this.lifetime_booking=lifetime;
+        this.lifetime_charger=lifetime;
         this.booked = new ArrayList<TimeWindow>();
         this.chargerSpots = new ArrayList<TimeWindow>(AgvExample.NUM_BATTERY);
     }
@@ -18,6 +20,7 @@ public class PheromoneStorage {
         for(TimeWindow tw1 : booked){
             if(tw1.end() < tw.begin() || tw1.begin() > tw.end()){
                 this.booked.add(tw);
+                this.lifetime_booking= 100;
                 System.out.println("Time window is possible. Booked!");
                 return 1;
             }
@@ -28,5 +31,18 @@ public class PheromoneStorage {
 
     public void setChargerSpots(int chargernumber, TimeWindow fulltw){
         this.chargerSpots.set(chargernumber,fulltw);
+        this.lifetime_charger= 100;
+    }
+
+    public int timepassing(int units){
+        this.lifetime_booking-=units;
+        this.lifetime_charger-=units;
+        if (this.lifetime_booking < 0){
+            this.booked.clear();
+        }
+        if (this.lifetime_charger < 0){
+            this.chargerSpots.clear();
+        }
+        return 0;
     }
 }
